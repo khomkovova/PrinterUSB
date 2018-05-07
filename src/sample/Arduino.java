7d2dbf265d3ba385286cdd6b317a9a06a7c43799
Unknown <khomko.vova@gmail.com>
2018-05-07 20:57:14 +0300

    Revert "Revert "123""
    This reverts commit d2249ed5017e17b455fc44b43cd52befbb77f34f.

--- src/sample/Arduino.java
+++ src/sample/Arduino.java
@@ -1,126 +1,36 @@
 package sample;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import gnu.io.CommPortIdentifier;
-import gnu.io.SerialPort;
-import gnu.io.SerialPortEvent;
-import gnu.io.SerialPortEventListener;
 
-import java.util.ArrayList;
-import java.util.Enumeration;
-public class Arduino  {
 
-    public SerialPort serialPort;
-    /** The port we’re normally going to use. */
-    private static final String PORT_NAMES[] = {
-"/dev/tty.usbserial-A9007UX1", // Mac OS X
-            "/dev/ttyUSB0", // Linux
-            "COM11", // Windows
-};
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
 
-public static BufferedReader input;
-public static OutputStream output;
-/** Milliseconds to block while waiting for port open */
-public static final int TIME_OUT = 2000;
-/** Default bits per second for COM port. */
-public static final int DATA_RATE = 9600;
+import java.io.*;
 
-public void initialize() {
-    SerialPort serialPort = null;
-    OutputStream outputStream = serialPort.getOutputStream().write();
-    Enumeration portEnum=CommPortIdentifier.getPortIdentifiers();
-    System.out.println("Serial ports:");
-    while (portEnum.hasMoreElements()) {
-        CommPortIdentifier portIdentifier= (CommPortIdentifier) portEnum.nextElement();
-        System.out.println("  " + portIdentifier.getName());
+public class Arduino implements SerialPortDataListener {
+
+    public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_RECEIVED; }
+
+    public int getPacketSize() { return 100; }
+
+    public void serialEvent(SerialPortEvent event)
+    {
+        byte[] newData = event.getReceivedData();
+        System.out.println("Received data of size: " + newData.length);
+        for (int i = 0; i < newData.length; ++i)
+            System.out.print((char)newData[i]);
+        System.out.println("\n");
     }
-////First, Find an instance of serial port as set in PORT_NAMES.
-//        while (portEnum.hasMoreElements()) {
-//        CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
-//        for (String portName : PORT_NAMES) {
-//        if (currPortId.getName().equals(portName)) {
-//        portId = currPortId;
-//        break;
-//        }
-//        }
-//        }
-//        if (portId == null) {
-//        System.out.println("Could not find COM port.");
-//        return;
-//        }
-//
-//        try {
-//// open serial port, and use class name for the appName.
-//        serialPort = (SerialPort) portId.open(this.getClass().getName(),
-//        TIME_OUT);
-//
-//// set port parameters
-//        serialPort.setSerialPortParams(DATA_RATE,
-//        SerialPort.DATABITS_8,
-//        SerialPort.STOPBITS_1,
-//        SerialPort.PARITY_NONE);
-//
-//// open the streams
-//        input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
-//        output = serialPort.getOutputStream();
-//        char ch = 1;
-//        output.write(ch);
-//
-//// add event listeners
-//        serialPort.addEventListener(this);
-//        serialPort.notifyOnDataAvailable(true);
-//        } catch (Exception e) {
-//        System.err.println(e.toString());
-//        }
-//        }
-//
-//        public synchronized void close() {
-//        if (serialPort != null) {
-//        serialPort.removeEventListener();
-//        serialPort.close();
-//        }
-//        }
-//
-//        public synchronized void serialEvent(SerialPortEvent oEvent) {
-//        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
-//        try {
-//        String inputLine=input.readLine();
-//        System.out.println(inputLine);
-//        } catch (Exception e) {
-//        System.err.println(e.toString());
-//        }
-//        }
-//
-//        }
-//
-//        public static synchronized void writeData(String data) {
-//        System.out.println("Sent: " + data);
-//        try {
-//        output.write(data.getBytes());
-//        } catch (Exception e) {
-//        System.out.println("could not write to port");
-//        }
-//        }
-//
 
-        }
-    public void main(){
-        Arduino arduino = new Arduino();
-        arduino.initialize();
-//        Thread t=new Thread() {
-//        public void run() {
-////the following line will keep this app alive for 1000 seconds,
-////waiting for events to occur and responding to them (printing incoming messages to console).
-//        try {Thread.sleep(1500);
-//        writeData("2");}
-//        catch (InterruptedException ie) {}
-//
-//        }
-//        };
-//        t.start();
-//        System.out.println("Started");
-    }
+public void sendComand(){
+    SerialPort comPort = SerialPort.getCommPorts()[0];
+    comPort.openPort();
+    Arduino listener = new Arduino();
+    comPort.addDataListener(listener);
+    try { Thread.sleep(5000); } catch (Exception e) { e.printStackTrace(); }
+    comPort.removeDataListener();
+    comPort.closePort();
+}
 }
 
